c6eaa62d6364d90348058e160575924a2a015c40
Aayush S C <animemandir40@gmail.com>
2023-09-29 14:49:40 +0530

    vercel try 4

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,6 @@ import animeRouter from "./routes";
 
 config();
 const app: express.Application = express();
-const PORT: number = Number(process.env.PORT) || 4000;
 
 app.use(morgan("dev"));
 app.use(corsConfig);
@@ -27,12 +26,4 @@ app.use("/anime", animeRouter);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`⚔️  api @ http://localhost:${PORT}`);
-});
-
-// don't sleep
-setInterval(() => {
-  console.log("HEALTHCHECK ;)", new Date().toLocaleString());
-  http.get("http://zoroapi-pink.vercel.app/health");
-}, 540000);
+module.exports = app;
